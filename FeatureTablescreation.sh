8f4e1be0e60f0a425a7bb64b0ff77dd11c9d6524
Daniela Deodato <dani.deodato2002@gmail.com>
2025-01-31 16:09:51 +0000

    Adds Feature table and representative sequences creation and chimera filtration script file

--- FeatureTablescreation.sh
+++ FeatureTablescreation.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
-#Este código é a segunda parte da análise metagenómica da minha tese usando o software qiime2
-#depois de realizado o demultiplexing e analisádo o "interactive Quality Plot" no ficheiro demux_{$data}.qzv 
-# durante o decorrer do script inserem-se os valores que se pede com base na análise do ficheiro demux_{$data}.qzv
-#Os argumentos usados neste script são o nome do diretório com os ficheiros originais e o ficheiro metadata
-#Criado por Daniela Deodato, Janeiro 2025
+#This code is the second part of the metagenomic analysis of my thesis, using the qiime2 software
+#After cuting the primers and analysing the "interactive Quality Plot" in the file trimmed-seqs_{$data}.qzv the perfered numbers in the configuration file are edited.
+#The script runs with the arguments: sequences directory name and metadata file
+#Copywrite Daniela Deodato, January 2025
 
 function help {
 echo ""
-echo "Este código é a segunda parte da análise metagenómica da minha tese usando o software qiime2"
-echo "depois de realizado o demultiplexing e analisádo o "interactive Quality Plot" no ficheiro demux_{$data}.qzv "
-echo "durante o decorrer do script inserem-se os valores que se pede com base na análise do ficheiro demux_{$data}.qzv."
-echo "Os argumentos usados neste script são o nome do diretório com os ficheiros originais e o ficheiro metadata"
+echo "This code is the second part of the metagenomic analysis of my thesis, using the qiime2 software"
+echo "After cuting the primers and analysing the "interactive Quality Plot" in the file trimmed-seqs_{$data}.qzv the perfered numbers in the configuration file are edited. "
+echo "#The script runs with the arguments: sequences directory name and metadata file"
 echo ""
 }
 
-#Variáveis
-data = $1 #nome do dirétório com as sequencias
-demux_file = trimmed-seqs_{$data}.qzv
-metadata = $2 #trocar pelo ficheiro metadata
+#Variables
+data=$1 #name of the directory with the samples
+cutadapt_file_viz=trimmed-seqs_{$data}.qzv
+cutadapt_file_art=trimmed-seqs_{$data}.qza
+metadata=$2 #metadata file
 
-num_min_seq = $(cat ConfigFile.yml | yq '.size.num_min_seq')
-trim_forward = $(cat ConfigFile.yml | yq '.trim.trim_forward')
-trim_reverse = $(cat ConfigFile.yml | yq '.trim.trim_reverse')
-trunc_forward = $(cat ConfigFile.yml | yq '.trunc.trunc_forward')
-trunc_reverse = $(cat ConfigFile.yml | yq '.trunc.trunc_reverse')
+num_min_seq=$(cat ConfigFile.yml | yq '.size.num_min_seq')
+trim_forward=$(cat ConfigFile.yml | yq '.trim.trim_forward')
+trim_reverse=$(cat ConfigFile.yml | yq '.trim.trim_reverse')
+trunc_forward=$(cat ConfigFile.yml | yq '.trunc.trunc_forward')
+trunc_reverse=$(cat ConfigFile.yml | yq '.trunc.trunc_reverse')
+outputDir=$(cat ConfigFile.yml | yq '.directory_name.output_dir_cutadapt')
 
-
-#Possiveis erros
-#verificar se o número de argumentos está correto
+#Possible errors
+#check the number of arguments
 if [ $# -ne 2 ];
  then
   help
@@ -37,16 +36,16 @@ if [ $# -ne 2 ];
   exit 1
  fi
 
-#verificar se a pasta existe
-if [ ! -e "$demux_file" ];
+#verify id the cutadapt file exists
+if [ ! -e "$cutdapt_file" ];
  then
   help
-  echo "ERRO: O ficheiro $demux_file não existe" > Erro.2.txt
+  echo "ERRO: O ficheiro $cutadapt_file não existe" > Erro.2.txt
   cat Erro.2.txt
   echo "Por favor verifique se o nome do ficheiro está correto e se está na diretoria"
   exit 1
  fi
-#verificar se o ficheiro metadata existe
+#verify if the metadata file exists
 if [ ! -e "$metadata" ];
  then
   help
@@ -56,18 +55,18 @@ if [ ! -e "$metadata" ];
   exit 1
  fi
 
- #filtrar amostras
+ #filtering samples
 
 qiime demux filter-samples \
-  --i-demux demux_{$data}.qza \
-  --m-metadata-file ./demux_{$data}/per-sample-fastq-counts.tsv \
+  --i-demux $cutadapt_file_art \
+  --m-metadata-file ./$outputDir/per-sample-fastq-counts.tsv \
   --p-where 'CAST([forward sequence count] AS INT) > {$num_min_seq}' \  #alterara para número de sequencias que se quer
-  --o-filtered-demux demux_{$data}.qza
+  --o-filtered-demux $cutadapt_file_art
 ###
 #denoising
 
 qiime dada2 denoise-paired \
-  --i-demultiplexed-seqs demux_{$data}.qza \ #alterar os seguintes parametros baseado nos nossos dados
+  --i-demultiplexed-seqs $cutadapt_file_art \ #alterar os seguintes parametros baseado nos nossos dados
   --p-trim-left-f $trim_forward \ 
   --p-trim-left-r $trim_reverse \
   --p-trunc-len-f $trunc_forward \
@@ -107,10 +106,12 @@ qiime feature-table filter-features \
   --i-table table_{$data}.qza \
   --m-metadata-file {$data}_uchime-dn-out/nonchimeras.qza \
   --o-filtered-table {$data}_uchime-dn-out/table-nonchimeric-wo-borderline.qza
+
 qiime feature-table filter-seqs \
   --i-data rep-seqs_{$data}.qza \
   --m-metadata-file {$data}_uchime-dn-out/nonchimeras.qza \
   --o-filtered-data {$data}_uchime-dn-out/rep-seqs-nonchimeric-wo-borderline.qza
+
 qiime feature-table summarize \
   --i-table {$data}_uchime-dn-out/table-nonchimeric-wo-borderline.qza \
   --o-visualization {$data}_uchime-dn-out/table-nonchimeric-wo-borderline.qzv
