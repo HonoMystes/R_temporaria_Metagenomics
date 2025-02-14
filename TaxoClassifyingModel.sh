c4d481df5dd356772c3ed3fa660b0068e51b5910
Daniela Deodato <dani.deodato2002@gmail.com>
2025-02-14 17:14:39 +0000

    Adds classification model script file

--- TaxoClassifyingModel.sh
+++ TaxoClassifyingModel.sh
@@ -1,70 +1,83 @@
 #!/bin/bash
 #This is the third part of the metagenomic analysis of my thesis, using qiime2
-#it will use the genome reference file SILVA https://data.qiime2.org/2024.10/common/silva-138-99-tax.qza
-#it will also use the featureTable[sequence] of the ref file
+#This script creates a classifing model specific of the region 16S of the SILVA database in qiime2  
 
+#variables
+classifier=$(cat ConfigFile.yml | yq '.taxonomy.classifier')
+ref_file_taxa=$(cat ConfigFile.yml | yq '.taxonomy.ref_taxa')
+ref_file_seq=$(cat ConfigFile.yml | yq '.taxonomy.ref_seq')
+ref_file_taxa_origin="origin_${ref_file_taxa}"
+ref_file_seq_origin="origin_${ref_file_seq}"
 
 #Get SILVA database
 qiime rescript get-silva-data \
     --p-version '138.2' \
     --p-target 'SSURef_NR99' \
-    --o-silva-sequences silva-138.2-ssu-nr99-rna-seqs.qza \
-    --o-silva-taxonomy silva-138.2-ssu-nr99-tax.qza
+    --o-silva-sequences "rna_$ref_file_seq_origin" \
+    --o-silva-taxonomy $ref_file_taxa_origin
 
 #making into FeatureTable[sequence]
 qiime rescript reverse-transcribe \
-    --i-rna-sequences silva-138.2-ssu-nr99-rna-seqs.qza \
-    --o-dna-sequences silva-138.2-ssu-nr99-seqs.qza
+    --i-rna-sequences "rna_$ref_file_seq_origin" \
+    --o-dna-sequences $ref_file_seq_origin
 
 #removing sequences that contain 5 or more ambiguous bases (IUPAC compliant ambiguity bases) and any homopolymers that are 8 or more bases in length. 
 qiime rescript cull-seqs \
-    --i-sequences silva-138.2-ssu-nr99-seqs.qza \
-    --o-clean-sequences silva-138.2-ssu-nr99-seqs-cleaned.qza
+    --i-sequences $ref_file_seq_origin \
+    --o-clean-sequences "cleaned_$ref_file_seq_origin"
 
 #filter ref file based on the classification to avoid references being lost
 qiime rescript filter-seqs-length-by-taxon \
-    --i-sequences silva-138.2-ssu-nr99-seqs-cleaned.qza \
-    --i-taxonomy silva-138.2-ssu-nr99-tax.qza \
+    --i-sequences "cleaned_$ref_file_seq_origin" \
+    --i-taxonomy $ref_file_taxa_origin \
     --p-labels Archaea Bacteria Eukaryota \
     --p-min-lens 900 1200 1400 \
-    --o-filtered-seqs silva-138.2-ssu-nr99-seqs-filt.qza \
-    --o-discarded-seqs silva-138.2-ssu-nr99-seqs-discard.qza
+    --o-filtered-seqs "filt_$ref_file_seq_origin" \
+    --o-discarded-seqs "discard_$ref_file_seq_origin"
 
 #dereplicate to remove redundant sequence data from the database
-#uniq approach retains identical sequence records that have differing taxonomies (ask)
+#uniq approach retains identical sequence records that have differing taxonomies
 qiime rescript dereplicate \
-    --i-sequences silva-138.2-ssu-nr99-seqs-filt.qza  \
-    --i-taxa silva-138.2-ssu-nr99-tax.qza \
+    --i-sequences "filt_$ref_file_seq_origin"  \
+    --i-taxa $ref_file_taxa_origin \
     --p-mode 'uniq' \
-    --o-dereplicated-sequences silva-138.2-ssu-nr99-seqs-derep-uniq.qza \
-    --o-dereplicated-taxa silva-138.2-ssu-nr99-tax-derep-uniq.qza
+    --o-dereplicated-sequences "derep_uniq_$ref_file_seq_origin" \
+    --o-dereplicated-taxa "derep_uniq_$ref_file_taxa_origin"
 
 #make amplicon-region specific classifier 
 
 #extract sequences
 qiime feature-classifier extract-reads \
-    --i-sequences silva-138.2-ssu-nr99-seqs-derep-uniq.qza \
+    --i-sequences "derep_uniq_$ref_file_seq_origin" \
     --p-f-primer GTGYCAGCMGCCGCGGTAA \
     --p-r-primer GGACTACNVGGGTWTCTAAT \
     --p-n-jobs 2 \
     --p-read-orientation 'forward' \
-    --o-reads silva-138.2-ssu-nr99-seqs-515f-806r.qza
+    --o-reads "derep_uniq_$ref_file_seq"
+#substitute the primers for the ones used in the sequencin (waiting for response)
 
 #dereplicate seqs 'cause the extracted amplicon regions may now be identical over this shorter region
 qiime rescript dereplicate \
-    --i-sequences silva-138.2-ssu-nr99-seqs-515f-806r.qza \
-    --i-taxa silva-138.2-ssu-nr99-tax-derep-uniq.qza \
+    --i-sequences "derep_uniq_$ref_file_seq" \
+    --i-taxa "derep_uniq_$ref_file_taxa_origin" \
     --p-mode 'uniq' \
-    --o-dereplicated-sequences silva-138.2-ssu-nr99-seqs-515f-806r-uniq.qza \
-    --o-dereplicated-taxa  silva-138.2-ssu-nr99-tax-515f-806r-derep-uniq.qza
+    --o-dereplicated-sequences $ref_file_seq \
+    --o-dereplicated-taxa  $ref_file_taxa
 
 #train classifier
 qiime feature-classifier fit-classifier-naive-bayes \
-    --i-reference-reads silva-138.2-ssu-nr99-seqs-515f-806r-uniq.qza \
-    --i-reference-taxonomy silva-138.2-ssu-nr99-tax-515f-806r-derep-uniq.qza \
-    --o-classifier silva-138.2-ssu-nr99-515f-806r-classifier.qza
+    --i-reference-reads $ref_file_seq \
+    --i-reference-taxonomy $ref_file_taxa \
+    --o-classifier $classifier
 
-classifier="silva-138.2-ssu-nr99-515f-806r-classifier.qza"
-ref_file="silva-138.2-ssu-nr99-tax-515f-806r-derep-uniq.qza"
-data=$1
-seqs="{$data}_uchime-dn-out/rep-seqs-nonchimeric-wo-borderline.qza"
+#test classifier
+qiime feature-classifier classify-sklearn \
+  --i-classifier $classifier \
+  --i-reads $ref_file_taxa \
+  --o-classification taxonomy.qza
+#tabulate to visualize in qiime 2
+qiime metadata tabulate \
+  --m-input-file taxonomy.qza \
+  --o-visualization taxonomy.qzv
+
+echo "check the "taxonomy.qzv" file in qiime2 to see the confidance of the classifier"
