541361761513c3282f92f0f67ef15600c11ab6aa
Daniela Deodato <dani.deodato2002@gmail.com>
2025-04-08 11:32:05 +0100

    Adds Feature table and representative sequences creation and chimera filtration script file

--- FeatureTablescreation.sh
+++ FeatureTablescreation.sh
@@ -11,27 +11,25 @@ set -e
 function help {
 echo ""
 echo "This code is the second part of the metagenomic analysis of my thesis, using the qiime2 software"
-echo "After cuting the primers and analysing the "interactive Quality Plot" in the file artifacts/trimmed-seqs_${data}.qzv the perfered numbers in the configuration file are edited. "
+echo "After cuting the primers and analysing the "interactive Quality Plot" in the file artifacts_PopLund/trimmed-seqs_${data}.qzv the perfered numbers in the configuration file are edited. "
 echo "the script runs with the argumentof the name of the population"
 echo ""
 }
 
 #Variables
 data=$1 #name of the directory with the samples
-cutadapt_file_art=artifact/trimmed-seqs_$data.qza
+cutadapt_file_art=artifact_$data/trimmed-seqs_$data.qza
 metadata=$(cat ConfigFile.yml | yq '.raw.metadata' | sed 's/\"//g')
+threads=$(cat ConfigFile.yml | yq '.raw.threads' | sed 's/\"//g')
 num_min_seq=$(cat ConfigFile.yml | yq '.size.num_min_seq' | sed 's/\"//g')
-trim_forward=$(cat ConfigFile.yml | yq '.trim.trim_forward' | sed 's/\"//g')
-trim_reverse=$(cat ConfigFile.yml | yq '.trim.trim_reverse' | sed 's/\"//g')
-trunc_forward=$(cat ConfigFile.yml | yq '.trunc.trunc_forward' | sed 's/\"//g')
-trunc_reverse=$(cat ConfigFile.yml | yq '.trunc.trunc_reverse' | sed 's/\"//g')
+length=$(cat ConfigFile.yml | yq '.trim.length' | sed 's/\"//g')
 outputDir=$(cat ConfigFile.yml | yq '.directory_name.output_dir_cutadapt' | sed 's/\"//g')
 persample=${outputDir}per-sample-fastq-counts.tsv
 freq_tbl=$(cat ConfigFile.yml | yq '.tables.freq_tbl' | sed 's/\"//g')
 freq_tbl_viz=$(cat ConfigFile.yml | yq '.tables.freq_tbl_viz' | sed 's/\"//g')
 seqs_rep=$(cat ConfigFile.yml | yq '.tables.seqs_rep' | sed 's/\"//g')
 seqs_rep_viz=$(cat ConfigFile.yml | yq '.tables.seqs_rep_viz' | sed 's/\"//g')
-fil=artifact/filter.qza
+fil=artifact_$data/filter.qza
 
 #Possible errors
 #check the number of arguments
@@ -74,16 +72,15 @@ echo "pass filter_sample"
 
 ###
 #Denoising
-qiime dada2 denoise-paired \
+qiime deblur denoise-16S \
   --i-demultiplexed-seqs $fil \
-  --p-trim-left-f $trim_forward \
-  --p-trim-left-r $trim_reverse \
-  --p-trunc-len-f $trunc_forward \
-  --p-trunc-len-r $trunc_reverse \
-  --o-table $freq_tbl \
+  --p-trim-length $length \
   --o-representative-sequences $seqs_rep \
-  --o-denoising-stats artifact/denoising-stats_$data.qza
-#alterar os seguintes parametros baseado nos nossos dados no --i-demultiplexed-seqs
+  --o-table $freq_tbl \
+  --p-sample-stats \
+  --o-stats artifact_$data/deblur_stats.qza \
+  --p-jobs-to-start $threads
+
 echo "pass denoising"
 
 #check if the files were created
@@ -114,20 +111,29 @@ qiime metadata tabulate \
   --m-input-file ${data}_uchime-dn-out/stats.qza \
   --o-visualization ${data}_uchime-dn-out/stats.qzv
 
+qiime deblur visualize-stats \
+  --i-deblur-stats artifact_$data/deblur_stats.qza \
+  --o-visualization vizualizations_$data/deblur_stats.qzv
+
 #Feature tables
 qiime feature-table filter-features \
   --i-table $freq_tbl \
   --m-metadata-file ${data}_uchime-dn-out/nonchimeras.qza \
-  --o-filtered-table $freq_tbl
+  --o-filtered-table artifact_$data/w_chimeraPopLund_freq_table.qza
 
 qiime feature-table filter-seqs \
   --i-data $seqs_rep \
   --m-metadata-file ${data}_uchime-dn-out/nonchimeras.qza \
-  --o-filtered-data $seqs_rep
+  --o-filtered-data artifact_$data/w_chimeraPopLund_rep_seqs.qza
 
 qiime feature-table summarize \
   --i-table $freq_tbl \
   --m-sample-metadata-file $metadata \
   --o-visualization $freq_tbl_viz
+
+qiime feature-table summarize \
+ --i-table artifact_$data/w_chimeraPopLund_freq_table.qza \
+ --m-sample-metadata-file $metadata \
+ --o-visualization vizualizations_$data/w_chimeraPopLund_freq_table.qzv
 
 echo "check $freq_tbl_viz to know what to do on script phyloDiv.sh"
